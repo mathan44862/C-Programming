db91ccee734b65af2efa8b8584b498baf2f4820a
Mathan M <108012913+mathan44862@users.noreply.github.com>
2024-03-09 14:56:47 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,14 @@
+//recursive factorial problem
+#include<stdio.h>
+int factorial(int fact){
+    if(fact == 0){
+        return 1;
+    }
+    return fact*factorial(fact-1);
+}
+void main(){
+    int fact;
+    printf("Enter the factorial number");
+    scanf("%d",&fact);
+    printf("%d",factorial(fact));
+}
